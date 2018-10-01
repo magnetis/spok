f03a323cdc0eefb5eb1abc3a54e40d3d089da02e
Zainal Mustofa <zainalmustof@gmail.com>
2018-10-01 23:21:03 +0700

    add holidays indonesian
    add holidays indonesian

--- lib/spok/workday.rb
+++ lib/spok/workday.rb
@@ -9,7 +9,7 @@ class Spok
   # module.
   module Workday
     # Public: Array of available calendars.
-    CALENDARS = %i(brasil bovespa portugal vietnam poland)
+    CALENDARS = %i(brasil bovespa portugal vietnam poland indonesia)
 
     # Public: Hash containing all holidays for each available calendar.
     HOLIDAYS = CALENDARS.map do |calendar|
