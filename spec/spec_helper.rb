0b5c82ee95cdf8592f7bc4bc2fa2d0312b410b6c
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-10-02 18:33:04 +0200

    allow repeats for vcr cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,4 +22,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.default_cassette_options = { allow_playback_repeats: true }
 end
