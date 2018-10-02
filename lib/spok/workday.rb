c407e335a81409b362532439ec56506e83271582
Alexander Zutikov <zvlex.dev@gmail.com>
2018-10-02 22:14:05 +0400

    Add default calendar (#33)
    * Add default calendar setting (https://github.com/magnetis/spok/issues/30)

--- lib/spok/workday.rb
+++ lib/spok/workday.rb
@@ -52,7 +52,7 @@ class Spok
     #   # => false
     #
     # Returns a boolean.
-    def self.restday?(date, calendar: :brasil)
+    def self.restday?(date, calendar: Spok.default_calendar)
       self.weekend?(date) || self.holiday?(date, calendar: calendar)
     end
 
@@ -68,7 +68,7 @@ class Spok
     #   # => true
     #
     # Returns a boolean.
-    def self.workday?(date, calendar: :brasil)
+    def self.workday?(date, calendar: Spok.default_calendar)
       !restday?(date, calendar: calendar)
     end
 
@@ -100,7 +100,7 @@ class Spok
     #   # => true
     #
     # Returns a boolean.
-    def self.holiday?(date, calendar: :brasil)
+    def self.holiday?(date, calendar: Spok.default_calendar)
       HOLIDAYS[calendar].include?(date.to_date)
     end
 
@@ -114,7 +114,7 @@ class Spok
     # Examples
     #   Spok::Workday.last_workday(Date.new(2012, 10, 21))
     #   # => #<Date: 2012-10-19 ((2456220j,0s,0n),+0s,2299161j)>
-    def self.last_workday(date, calendar: :brasil)
+    def self.last_workday(date, calendar: Spok.default_calendar)
       return date if workday?(date, calendar: calendar)
 
       last_workday((date - 1.day), calendar: calendar)
@@ -130,7 +130,7 @@ class Spok
     # Examples
     #   Spok::Workday.next_workday(Date.new(2012, 10, 21))
     #   # => #<Date: 2012-10-19 ((2456220j,0s,0n),+0s,2299161j)>
-    def self.next_workday(date, calendar: :brasil)
+    def self.next_workday(date, calendar: Spok.default_calendar)
       return date if workday?(date, calendar: calendar)
 
       next_workday((date + 1.day), calendar: calendar)
