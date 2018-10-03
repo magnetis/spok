c8bc141142789409763d3001d18aaf8decf61817
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-10-03 13:57:27 +0200

    add YamlExporter in generate_holiday_calendar.rb

--- helpers/generate_holiday_calendar.rb
+++ helpers/generate_holiday_calendar.rb
@@ -1,20 +1,27 @@
 require 'json'
 require 'yaml'
 
+class YamlExporter
+  def export(file, holidays)
+    holidays_hash = { File.basename(file.path, '.yml') => holidays }
+    file.write(holidays_hash.to_yaml)
+  end
+end
+
 class GenerateHolidayCalendar
-  def initialize(holiday_fetcher:)
+  def initialize(holiday_fetcher:, exporter: YamlExporter.new)
     @holiday_fetcher = holiday_fetcher
+    @exporter = exporter
   end
 
   def generate(file:, country_code:, years:)
     holidays = years.flat_map(&fetch_holidays(country_code))
-    holidays_hash = { country_code => holidays }
-    file.write(holidays_hash.to_yaml)
+    exporter.export(file, holidays)
   end
 
   private
 
-  attr_reader :holiday_fetcher
+  attr_reader :holiday_fetcher, :exporter
 
   def fetch_holidays(country_code)
     lambda do |year|
