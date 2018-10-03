48243f839fe2a1d3ed970473c7059625ac173546
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-10-03 15:09:04 +0200

    test refactor

--- spec/helpers/generate_holiday_calendar_spec.rb
+++ spec/helpers/generate_holiday_calendar_spec.rb
@@ -4,76 +4,55 @@ require_relative '../../helpers/generate_holiday_calendar'
 require_relative '../../helpers/holiday_fetcher'
 
 describe GenerateHolidayCalendar do
-  subject { described_class.new(holiday_fetcher: holiday_fetcher) }
+  subject { described_class.new(holiday_fetcher: holiday_fetcher, exporter: exporter) }
 
   let(:file) { Tempfile.new }
+  let(:country_code) { 'PL' }
+  let(:holiday_2018_05_01) { HolidayFetcher::Holiday.new(Date.new(2018, 5, 1)) }
+  let(:holiday_2017_05_01) { HolidayFetcher::Holiday.new(Date.new(2017, 5, 1)) }
+  let(:holiday_2016_05_01) { HolidayFetcher::Holiday.new(Date.new(2016, 5, 1)) }
 
-  context 'integration test', vcr: { cassette_name: 'PL_2000' } do
-    let (:holiday_fetcher) { HolidayFetcher.new }
-    let(:country_code) { 'PL' }
+  let(:exporter) do
+    double.tap do |exporter|
+      allow(exporter).to receive(:export).with(file, any_args)
+    end
+  end
+
+  context 'for single year' do
+    let(:holiday_fetcher) do
+      double.tap do |fetcher|
+        allow(fetcher).to receive(:fetch).with(country_code, 2018).and_return(
+          [holiday_2018_05_01]
+        )
+      end
+    end
 
     it 'return data' do
-      subject.generate(file: file, country_code: country_code, years: [2000])
+      expect(exporter).to receive(:export).with(file, [holiday_2018_05_01])
 
-      file.rewind
-      expect(File.read(file.path)).to start_with(<<~YAML)
-        ---
-        #{File.basename(file.path, '.yml')}:
-        - 2000-01-01
-      YAML
+      subject.generate(file: file, country_code: country_code, years: [2018])
     end
   end
 
-  context 'unit test' do
-    let(:country_code) { 'PL' }
-
-    context 'for single year' do
-      let(:holiday_fetcher) do
-        double.tap do |fetcher|
-          allow(fetcher).to receive(:fetch).with(country_code, 2018).and_return(
-            [HolidayFetcher::Holiday.new(Date.new(2018, 5, 1))]
-          )
-        end
-      end
-      it 'return data' do
-        subject.generate(file: file, country_code: country_code, years: [2018])
-
-        file.rewind
-        expect(File.read(file.path)).to eq(<<~YAML)
-          ---
-          #{File.basename(file.path, '.yml')}:
-          - 2018-05-01
-        YAML
+  context 'for range of years' do
+    let(:holiday_fetcher) do
+      double.tap do |fetcher|
+        allow(fetcher).to receive(:fetch).with(country_code, 2018).and_return(
+          [holiday_2018_05_01]
+        )
+        allow(fetcher).to receive(:fetch).with(country_code, 2017).and_return(
+          [holiday_2017_05_01]
+        )
+        allow(fetcher).to receive(:fetch).with(country_code, 2016).and_return(
+          [holiday_2016_05_01]
+        )
       end
     end
-    context 'for range of years' do
-      let(:holiday_fetcher) do
-        double.tap do |fetcher|
-          allow(fetcher).to receive(:fetch).with(country_code, 2018).and_return(
-            [HolidayFetcher::Holiday.new(Date.new(2018, 5, 1))]
-          )
-          allow(fetcher).to receive(:fetch).with(country_code, 2017).and_return(
-            [HolidayFetcher::Holiday.new(Date.new(2017, 5, 1))]
-          )
-          allow(fetcher).to receive(:fetch).with(country_code, 2016).and_return(
-            [HolidayFetcher::Holiday.new(Date.new(2016, 5, 1))]
-          )
-        end
-      end
 
-      it 'return data' do
-        subject.generate(file: file, country_code: country_code, years: (2016..2018))
+    it 'return data' do
+      expect(exporter).to receive(:export).with(file, [holiday_2016_05_01, holiday_2017_05_01, holiday_2018_05_01])
 
-        file.rewind
-        expect(File.read(file.path)).to eq(<<~YAML)
-          ---
-          #{File.basename(file.path, '.yml')}:
-          - 2016-05-01
-          - 2017-05-01
-          - 2018-05-01
-        YAML
-      end
+      subject.generate(file: file, country_code: country_code, years: (2016..2018))
     end
   end
-
 end
